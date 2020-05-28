fefbdfa76f434f30b9e98b93961323d6e3bbc329
Leha228 <ryabin.01@mail.ru>
2020-05-28 20:44:45 +0500

    Upd

--- o_comp.php
+++ o_comp.php
@@ -9,56 +9,67 @@
             <hr style="width: 45px; margin: 30px auto">
             <div class="container">
         <div class="content__box content__box_not-graph">
-                <div>
-                    <br><br>
-                    <h5 class="upper animated wow slideInLeft">О компании</h5><br>
-                    <br>
-                    <p class="animated wow slideInLeft">Статус: Действующее</p>
-                    <br>
-                    <p class="animated wow slideInLeft">Дата регистрации: 23 августа 2004 года</p>
-                    <br>
-                    <p class="animated wow slideInLeft">ОГРН: 1047796624061</p>
-                    <br>
-                    <p class="animated wow slideInLeft">ИНН: 7725517455</p>
-                    <br>
-                    <p class="animated wow slideInLeft">КПП: 631801001</p>
-                    <br>
-                    <p class="animated wow slideInLeft">ОКПО: 73872570</p>
-                    <br><br>
-                    <h5 class="upper animated wow slideInLeft">Юридический адрес</h5>
-                    <br>
-                    <p class="animated wow slideInLeft">443022, Самарская обл, город Самара, проезд Мальцева, дом 7е, ОФИС 002</p>
-                    <br><br>
-                    <h5 class="upper animated wow slideInLeft">Генеральный Директор</h5>
-                    <br>
-                    <p class="animated wow slideInLeft">Лепихин Александр Геннадьевич</p><br>
-                    <br>
-                    <h5 class="upper animated wow slideInLeft">Учредители</h5>
-                    <br>
-                    <p class="animated wow slideInLeft">Лепихин Александр Геннадьевич</p>
-                    <p class="animated wow slideInLeft">Уставный капитал: 5 000 000,00 руб</p>
-                    <br><br>
-                    <h5 class="upper animated wow slideInLeft">Держатель реестра</h5>
-                    <br>
-                    <p class="animated wow slideInLeft">АКЦИОНЕРНОЕ ОБЩЕСТВО ВТБ РЕГИСТРАТОР, 5610083568</p>
-                    <br><br>
-                    <h5 class="upper animated wow slideInLeft">Основной вид деятельности</h5>
-                    <br>
-                    <p class="animated wow slideInLeft">28.2 производство прочих машин и оборудования общего назначения</p>
-                    <br><br>
-                    <h5 class="upper animated wow slideInLeft">Налоговый орган</h5>
-                    <br>
-                    <p class="animated wow slideInLeft">Инспекция Федеральной Налоговой Службы По Советскому Району Г. Самары</p>
-                    <p class="animated wow slideInLeft">Дата постановки на учет: 26.08.2019</p>
-                    <br><br>
-                    <h5 class="upper animated wow slideInLeft">Регистрация во внебюджетных фондах</h5>
-                    <br>
-                    <p class="animated wow slideInLeft">Фонд ПФР (28.08.2019)</p>
-                    <p class="animated wow slideInLeft">Фонд ФСС (27.08.2019)</p>
-                    
+        <div class="content__box content__box_not-graph">
+                <div class="img__content" style="display: flex; justify-content: space-between;">
+                    <div>
+                        <img src="img/about.png">
+                    </div>
+                    <div>
+                        <br><br>
+                        <h5 class="upper animated wow slideInLeft">О компании</h5><br>
+                        <br>
+                        <p class="animated wow slideInLeft">Статус: Действующее</p>
+                        <br>
+                        <p class="animated wow slideInLeft">Дата регистрации: 23 августа 2004 года</p>
+                        <br>
+                        <p class="animated wow slideInLeft">ОГРН: 1047796624061</p>
+                        <br>
+                        <p class="animated wow slideInLeft">ИНН: 7725517455</p>
+                        <br>
+                        <p class="animated wow slideInLeft">КПП: 631801001</p>
+                        <br>
+                        <p class="animated wow slideInLeft">ОКПО: 73872570</p>
+                        <br><br>
+                        <h5 class="upper animated wow slideInLeft">Юридический адрес</h5>
+                        <br>
+                        <p class="animated wow slideInLeft">443022, Самарская обл, город Самара, проезд Мальцева, дом 7е, ОФИС 002</p>
+                        <br><br>
+                    </div>
                 </div>
-           
-            
+                <div class="img__content" style="display: flex; justify-content: space-between; margin-top:50px;">
+                    <div>
+                        <h5 class="upper animated wow slideInLeft">Генеральный Директор</h5>
+                        <br>
+                        <p class="animated wow slideInLeft">Лепихин Александр Геннадьевич</p><br>
+                        <br>
+                        <h5 class="upper animated wow slideInLeft">Учредители</h5>
+                        <br>
+                        <p class="animated wow slideInLeft">Лепихин Александр Геннадьевич</p>
+                        <p class="animated wow slideInLeft">Уставный капитал: 5 000 000,00 руб</p>
+                        <br><br>
+                        <h5 class="upper animated wow slideInLeft">Держатель реестра</h5>
+                        <br>
+                        <p class="animated wow slideInLeft">АКЦИОНЕРНОЕ ОБЩЕСТВО ВТБ РЕГИСТРАТОР, 5610083568</p>
+                        <br><br>
+                        <h5 class="upper animated wow slideInLeft">Основной вид деятельности</h5>
+                        <br>
+                        <p class="animated wow slideInLeft">28.2 производство прочих машин и оборудования общего назначения</p>
+                        <br><br>
+                        <h5 class="upper animated wow slideInLeft">Налоговый орган</h5>
+                        <br>
+                        <p class="animated wow slideInLeft">Инспекция Федеральной Налоговой Службы По Советскому Району Г. Самары</p>
+                        <p class="animated wow slideInLeft">Дата постановки на учет: 26.08.2019</p>
+                        <br><br>
+                        <h5 class="upper animated wow slideInLeft">Регистрация во внебюджетных фондах</h5>
+                        <br>
+                        <p class="animated wow slideInLeft">Фонд ПФР (28.08.2019)</p>
+                        <p class="animated wow slideInLeft">Фонд ФСС (27.08.2019)</p>
+                    </div>
+                    <div>
+                        <img src="img/about.png">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
             </div>
