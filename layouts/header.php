5d7af7a44ca2bfff70e93c6f710a7186c989593b
Leha228 <ryabin.01@mail.ru>
2020-05-28 10:09:58 +0500

    Update

---
+++ layouts/header.php
@@ -0,0 +1,18 @@
+<body>
+<header class="header1">
+    <div class="container header__container" >
+        <nav class="header__nav">
+            <ul>
+                <li><a href="index.php">Главная</a></li>
+                <li><a href="uslugi.php">Услуги</a></li>
+                <li><a href="contacts.php">Контакты</a></li>
+                <li><a href="o_comp.php">Документация</a></li>
+            </ul>
+            <a id="order_button" class="go_to header__order" href="#order">Заказать звонок</a>
+        </nav>
+        <div class="header__titled1" id="titled">
+            <h2>МЕХТА</h2>
+            <h5 class="white">Акционерное общество</h5>
+        </div>
+    </div>
+</header>
--- o_comp.php
+++ o_comp.php
@@ -1,24 +1,7 @@
 <?php 
     require 'layouts/head.php';
+    require 'layouts/header.php';
 ?>
-<body>
-<header class="header1">
-    <div class="container header__container" >
-        <nav class="header__nav">
-            <ul>
-                <li><a href="index.php">Главная</a></li>
-                <li><a href="uslugi.php">Услуги</a></li>
-                <li><a href="contacts.php">Контакты</a></li>
-                <li><a href="o_comp.php">Документация</a></li>
-            </ul>
-            <a id="order_button" class="go_to header__order" href="#order">Заказать звонок</a>
-        </nav>
-        <div class="header__titled1" id="titled">
-            <h2>МЕХТА</h2>
-            <h5 class="white">Акционерное общество</h5>
-        </div>
-    </div>
-</header>
 <section class="content content_grey" id="about">
     <div class="container">
         <div class="content__box">
